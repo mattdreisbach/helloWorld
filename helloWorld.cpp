e2f2b8360a0f02d775c1c41e8b62dc5ba0818e90
mattdr <mattdr@umich.edu>
2017-03-16 13:40:22 -0400

    this is my first commit

---
+++ helloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	cout << "Hello woooorld" << endl;
+	if (TRUE)
+	{
+		cout << "more because true" << endl;
+	}
+	else
+	{
+		cout << "wont get here" << endl;
+	}	
+	return 0;
+}
+
